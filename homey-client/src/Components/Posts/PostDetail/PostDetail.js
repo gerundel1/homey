ee1fa6dd3cd791afb96e268ad97ead3bf0f85640
German Malikov <germalikov@gmail.com>
2022-04-11 10:45:05 -0400

    fix image slider

--- homey-client/src/Components/Posts/PostDetail/PostDetail.js
+++ homey-client/src/Components/Posts/PostDetail/PostDetail.js
@@ -1,12 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import { UserContext } from "../../../App";
 import axios from "axios";
@@ -16,61 +10,39 @@ import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 import "./PostDetail.css";
 
 function PostDetail(props) {
-    const { userName, type, userEmail } = useContext(UserContext);
+    const { userName, type } = useContext(UserContext);
     const [product, setProduct] = useState({});
+    const [images, setImages] = useState([]);
     const productId = props.id;
-    const [image, setImage] = useState("");
 
     // --------------
     // slider data
-    const [productImages, setProductImages] = useState([]);
-
-    // temperaory static data
-    const prodImages = [
-        {
-            image: "https://images.unsplash.com/photo-1649331352505-8117d066f361?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=600&q=60",
-        },
-        {
-            image: "https://images.unsplash.com/photo-1599420186946-7b6fb4e297f0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-        },
-        {
-            image: "https://images.unsplash.com/photo-1649274617106-d8df57b78aa3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
-        },
-    ];
 
     const [current, setCurrent] = useState(0);
-    const length = prodImages.length;
 
     const nextSlide = () => {
-        setCurrent(current === length - 1 ? 0 : current + 1);
+        setCurrent(current === images.length - 1 ? 0 : current + 1);
         console.log(current);
     };
 
     const prevSlide = () => {
-        setCurrent(current === 0 ? length - 1 : current - 1);
+        setCurrent(current === 0 ? images.length - 1 : current - 1);
         console.log(current);
     };
 
     // --------------
 
-    useEffect(async () => {
-        await axios
-            .get(`http://localhost:8080/api/product/${productId}`)
-            .then((result) => {
-                setProduct(result.data);
-                setImage(
-                    `http://localhost:8080/api/product/image/${result.data.images[0]}`
-                );
-
-                // set the images into variable
-                // Not working
-                setProductImages(
-                    `http://localhost:8080/api/product/image/${result.data.images}`
-                );
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+    useEffect(() => {
+        async function getProduct () {
+            try{
+            const data = await axios.get(`http://localhost:8080/api/product/${productId}`);
+            setProduct(data.data);
+            setImages(data.data.images);
+             }catch(err){
+                    console.log(err);
+             }
+        }
+        getProduct();
     }, []);
 
     const handleSubmit = (event) => {
@@ -112,9 +84,6 @@ function PostDetail(props) {
     return (
         <div className="postdetail-container">
             <h1>{product.name}</h1>
-            {/* <img src={image} alt="food image" width="600" height="400" /> */}
-
-            {/* Image slider */}
 
             <section className="slider">
                 <FaArrowAltCircleLeft
@@ -125,7 +94,7 @@ function PostDetail(props) {
                     className="right-arrow"
                     onClick={nextSlide}
                 />
-                {prodImages.map((slide, index) => {
+                {images.map((slide, index) => {
                     return (
                         <div
                             className={
@@ -135,7 +104,7 @@ function PostDetail(props) {
                         >
                             {index === current && (
                                 <img
-                                    src={image}
+                                    src={`http://localhost:8080/api/product/image/${slide}`}
                                     alt="product image"
                                     className="image"
                                 />
